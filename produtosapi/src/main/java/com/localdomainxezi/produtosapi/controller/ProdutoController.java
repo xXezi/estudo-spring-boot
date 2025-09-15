da04bc77103d3aba8580e9ffc1c33c25cb487366
William Roberti Moreira <willroberti04@hotmail.com>
2025-09-15 18:59:24 -0300

    atualizando e buscando um produto

--- produtosapi/src/main/java/com/localdomainxezi/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/localdomainxezi/produtosapi/controller/ProdutoController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 import java.util.UUID;
 
 import com.localdomainxezi.produtosapi.model.Produto;
@@ -42,5 +46,16 @@ public class ProdutoController {
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable("id") String id) {
 		produtoRepository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+		produto.setId(id);
+		produtoRepository.save(produto);
+	}
+	
+	@GetMapping
+	public List<Produto> buscar(@RequestParam("nome") String nome){
+		return produtoRepository.findByNome(nome);
 	}
 }
--- produtosapi/src/main/java/com/localdomainxezi/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/com/localdomainxezi/produtosapi/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package com.localdomainxezi.produtosapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.localdomainxezi.produtosapi.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, String>{
+	List<Produto> findByNome(String nome);
 }
