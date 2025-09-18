484649a20f738194a037ecd1cdbcd6142dcd021c
William Roberti Moreira <willroberti04@hotmail.com>
2025-09-17 22:15:14 -0300

    criando o projeto arquitetura spring

---
+++ arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/ArquiteturaspringApplication.java
@@ -0,0 +1,13 @@
+package com.localxezi.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaspringApplication.class, args);
+	}
+
+}
---
+++ arquiteturaspring/src/test/java/com/localxezi/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localxezi.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
