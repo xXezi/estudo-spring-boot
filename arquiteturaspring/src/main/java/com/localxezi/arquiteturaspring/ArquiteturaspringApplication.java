dd70774471fb3df9ef40f6a7c70f9aa1e1333686
William Roberti Moreira <willroberti04@hotmail.com>
2025-09-22 23:48:30 -0300

    adicionando ajustes no arquivo pom

--- arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/ArquiteturaspringApplication.java
+++ arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/ArquiteturaspringApplication.java
@@ -9,5 +9,4 @@ public class ArquiteturaspringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArquiteturaspringApplication.class, args);
 	}
-
 }
--- arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -1,7 +1,9 @@
 package com.localxezi.arquiteturaspring.montadora.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.localxezi.arquiteturaspring.montadora.CarroStatus;
@@ -10,11 +12,13 @@ import com.localxezi.arquiteturaspring.montadora.HondaHRV;
 import com.localxezi.arquiteturaspring.montadora.Motor;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 	
 	@Autowired
 	private Motor motor;
 	
+	@PostMapping
 	public CarroStatus ligarCarro(@RequestBody Chave chave) {
 		var carro = new HondaHRV(motor);
 		return carro.darIgnicao(chave);
