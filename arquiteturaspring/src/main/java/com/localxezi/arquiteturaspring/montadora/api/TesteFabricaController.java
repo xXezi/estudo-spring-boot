48a838773327e903c4aeb1c7ab3e481ea8df8825
William Roberti Moreira <willroberti04@hotmail.com>
2025-09-23 21:26:50 -0300

    utilizando qualifier

--- arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -1,6 +1,7 @@
 package com.localxezi.arquiteturaspring.montadora.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.localxezi.arquiteturaspring.montadora.Motor;
 public class TesteFabricaController {
 	
 	@Autowired
+	@Qualifier("motorTurbo")
 	private Motor motor;
 	
 	@PostMapping
--- arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ arquiteturaspring/src/main/java/com/localxezi/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -9,14 +9,36 @@ import com.localxezi.arquiteturaspring.montadora.TipoMotor;
 @Configuration
 public class MontadoraConfiguration {
 	
-	@Bean
-	Motor motor() {
+	@Bean(name = "motorAspirado")
+	Motor motorAspirado() {
 		var motor = new Motor();
 		motor.setCavalos(120);
 		motor.setCilindros(4);
 		motor.setModelo("XPTO-0");
 		motor.setLitragem(2.0);
 		motor.setTipo(TipoMotor.ASPIRADO);
+		return motor;
+	}
+	
+	@Bean(name = "motorEletrico")
+	Motor motorEletrico() {
+		var motor = new Motor();
+		motor.setCavalos(110);
+		motor.setCilindros(3);
+		motor.setModelo("TH-40");
+		motor.setLitragem(1.4);
+		motor.setTipo(TipoMotor.ELETRICO);
+		return motor;
+	}
+	
+	@Bean(name = "motorTurbo")
+	Motor motorTurbo() {
+		var motor = new Motor();
+		motor.setCavalos(180);
+		motor.setCilindros(4);
+		motor.setModelo("XPTO-0");
+		motor.setLitragem(1.5);
+		motor.setTipo(TipoMotor.TURBO);
 		return motor;
 	}
 }
